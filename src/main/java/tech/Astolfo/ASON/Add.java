2b854735eed4d275b228ca17eaa25d9f1f813797
Computerfile <55207418+Computerfile@users.noreply.github.com>
2020-10-05 23:49:24 +0200

    🎨 Cleaned up the code, marked two unused methods as depecrated made the "Add" class public and more user friendly bu avoiding public methods and using constructors.

--- src/main/java/tech/Astolfo/ASON/Add.java
+++ src/main/java/tech/Astolfo/ASON/Add.java
@@ -8,31 +8,31 @@ import java.util.HashSet;
 
 public class Add {
 
-    public Add(String key, String string){
-
+    public Add(String key, String string, File file) throws IOException {
+        UploadToFile(key, string, file);
     }
 
-    public Add(String key, int number){
-
+    public Add(String key, int number, File file) throws IOException{
+        UploadToFile(key, number, file);
     }
 
-    public Add(String key, HashMap<Object, Object> map){
-
+    public Add(String key, HashMap<Object, Object> map, File file) throws IOException{
+        UploadToFile(key, map, file);
     }
 
-    public Add(String key, HashSet<String> set){
-
+    public Add(String key, HashSet<String> set, File file) throws IOException{
+        UploadToFile(key, set, file);
     }
 
-    public Add(String key, boolean bool){
-
+    public Add(String key, boolean bool, File file) throws IOException{
+        UploadToFile(key, bool, file);
     }
 
-    public Add(String key){
-
+    public Add(String key, File file) throws IOException{
+        UploadToFile(key, null, file);
     }
 
-    void UploadToFile(String key, Object obj, File file) throws IOException {
+    private void UploadToFile(String key, Object obj, File file) throws IOException {
         FileWriter write = new FileWriter(file);
         write.append("'" + key + "'" + obj);
     }
--- src/main/java/tech/Astolfo/ASON/JSONWriter.java
+++ src/main/java/tech/Astolfo/ASON/JSONWriter.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+
+@Deprecated
 class JSONWriter {
 
     OpenFile op;
