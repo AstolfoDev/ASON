bc926261d448ed14e502dfc01db14fcc47fba517
Computerfile <feddag2022@ish.dk>
2020-10-15 16:29:07 +0200

    🔨 Removed unnecessary code in the Parser Class now directly parses the Object to it's appropriate primitive data type without running through multiple functions

--- src/main/java/tech/Astolfo/ASON/JSON/AParser.java
+++ src/main/java/tech/Astolfo/ASON/JSON/AParser.java
@@ -5,50 +5,21 @@ import java.util.HashSet;
 import java.util.function.Function;
 
 class AParser {
-
     /**
      * @param Object the input parameters will be an Object
-     * return HashMap the function will output a HashMap
-     */
-
-    Function<Object, HashMap> convertHashMaps = (o -> (HashMap) o);
-
-    /**
-     * @param Object the input parameters will be an Object
-     * return HashSett the function will output a HashSet
-     */
-
-    Function<Object, HashSet> convertSets = (o -> (HashSet) o);
-
-    /**
-     * @param Object the input parameters will be an Object
-     * return String the function will output a String
-     */
-
-    Function<Object, String> convertString = (o -> (String) o);
-
-    /**
-     * @param Object the input parameters will be an Object
-     * return Object in this case the function will output an Object
-     */
-
-    Function<Object, Object> returnObject = (o -> o);
-
-    /**
-     * @param Object the input parameters will be an Object
-     * return Undefined output meaning the output can be anything
-     *               changing it according to what type the Object can be converted too
+     * return The Output will vary according to what the object type is this is 
+     *              for easier classification of the object within the different possible types
      */
 
     protected Function<Object, ?> GetTypes = (obj -> {
         if(obj instanceof HashMap){
-            return convertHashMaps.apply(obj);
+            return (HashMap) obj;
         }else if(obj instanceof HashSet){
-            return convertSets.apply(obj);
+            return (HashSet) obj;
         }else if(obj instanceof String){
-            return convertString.apply(obj);
+            return (String) obj;
         }else {
-            return returnObject.apply(obj);
+            return obj;
         }
     });
 }
