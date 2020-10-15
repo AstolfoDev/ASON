ab7adb30a15fbfc5e51a0ad3031fd349eae5caef
Computerfile <feddag2022@ish.dk>
2020-10-15 17:12:38 +0200

    🔨  Added a functioning parser that (according to the data type) will append the text given to the given file in an appropriate JSON format

--- src/main/java/tech/Astolfo/ASON/JSON/AParser.java
+++ src/main/java/tech/Astolfo/ASON/JSON/AParser.java
@@ -1,25 +1,102 @@
 package tech.Astolfo.ASON.JSON;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 class AParser {
     /**
-     * @param Object the input parameters will be an Object
-     * return The Output will vary according to what the object type is this is 
-     *              for easier classification of the object within the different possible types
+     * @param f: At the moment an empty file, however this file will be equal to the final file that needs to be edited
+     * @param HashI: An AtomicInteger made atomic to make it accessible during lambda expressions
+     *             equal to the size of the HashMap value of the Object
+     * @param SetI: An AtomicInteger made atomic to make it accessible during lambda expressions
+     *            equal to the size of the HasSet value of the Object
+     * @param fw: An Instance of the FileWriter class that is used to edit the file f
      */
 
-    protected Function<Object, ?> GetTypes = (obj -> {
-        if(obj instanceof HashMap){
-            return (HashMap) obj;
-        }else if(obj instanceof HashSet){
-            return (HashSet) obj;
-        }else if(obj instanceof String){
-            return (String) obj;
-        }else {
-            return obj;
+    File f;
+    AtomicInteger HashI = new AtomicInteger();
+    AtomicInteger SetI = new AtomicInteger();
+    FileWriter fw;
+
+    /**
+     * @param file the file that needs to be edited and parsed into JSON which will be equal to the previously defined
+     *             file f
+     */
+
+    AParser(File file){
+        this.f = file;
+    }
+
+
+
+    /**
+     * @param Object the input parameters will be an Object
+     *               this will be parsed into it's according data types and will be managed accordingly
+     * @return true if the code is successful it will return true else it will throw an error
+     */
+
+    Function<Object, ?> GetTypes = (obj -> {
+
+        try {
+            fw = new FileWriter(f);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        if(obj instanceof HashMap){
+            ((HashMap) obj).forEach((k,v)-> {
+                HashI.set(((HashMap) obj).size());
+                this.GetTypes.apply(k);
+                try {
+                    fw.append(":");
+
+                    this.GetTypes.apply(v);
+
+                    if(((HashMap) obj).size() == HashI.get()){
+                        fw.append(";");
+                    }else{
+                        fw.append(",");
+                    }
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }else if(obj instanceof HashSet){
+            SetI.set(((HashSet) obj).size());
+            ((HashSet) obj).forEach(v -> {
+                try{
+                    this.GetTypes.apply(obj);
+
+                    if(((HashSet) obj).size() == SetI.get()){
+                        fw.append(";");
+                    }else {
+                        fw.append(",");
+                    }
+
+                    SetI.getAndAdd(1);
+
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            });
+        }else if(obj instanceof String){
+            try{
+                fw.append("'" + obj.toString() + "'");
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }else {
+            try{
+                fw.append(obj.toString());
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return true;
     });
 }
